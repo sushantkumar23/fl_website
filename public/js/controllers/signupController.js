eee73c12939de6e3a079b8eae702ef55a6d4d516
sushantkumar23 <sushantkumar23@gmail.com>
2015-08-07 12:57:58 +0530

    Login modal added.

---
+++ public/js/controllers/signupController.js
@@ -0,0 +1,13 @@
+
+function SignupController()
+{
+// redirect to homepage when cancel button is clicked //
+	$('#account-form-btn1').click(function(){ window.location.href = '/';});
+
+// redirect to homepage on new account creation, add short delay so user can read alert window //
+	$('.modal-alert #ok').click(function(){ setTimeout(function(){window.location.href = '/';}, 300)});
+
+	$('#signup_btn').click(function(){ 
+		$('#account-form-container').modal('show');
+	});
+}
---
+++ public/js/views/signup.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+	
+	var av = new AccountValidator();
+	
+	$('#account-form').ajaxForm({
+		beforeSubmit : function(formData, jqForm, options){
+			return av.validateForm();
+		},
+		success	: function(responseText, status, xhr, $form){
+			if (status == 'success') $('.modal-alert').modal('show');
+		},
+		error : function(e){
+			if (e.responseText == 'email-taken'){
+			    av.showInvalidEmail();
+			}	else if (e.responseText == 'username-taken'){
+			    av.showInvalidUserName();
+			}
+		}
+	});
+	$('#name-tf').focus();
+	
+// customize the account signup form //
+	
+	
+// setup the alert that displays when an account is successfully created //
+
+	$('.modal-alert').modal({ show : false, keyboard : false, backdrop : 'static' });
+	$('.modal-alert .modal-header h3').text('Success!');
+	$('.modal-alert .modal-body p').html('Your account has been created.</br>Click OK to return to the login page.');
+
+})
